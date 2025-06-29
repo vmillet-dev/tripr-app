6878186eb982d2e46ab8da28c183be143ce8fc3d
Valentin MILLET <valentin.millet39@gmail.com>
2025-06-29 21:38:52 +0200

    use springboot lib bundle in build.gradle.kts

--- backend/infrastructure/build.gradle.kts
+++ backend/infrastructure/build.gradle.kts
@@ -12,12 +12,7 @@ dependencies {
 
     runtimeOnly(libs.postgresql)
 
-    implementation(libs.springboot.starter.web)
-    implementation(libs.springboot.starter.data.jpa)
-    implementation(libs.springboot.starter.security)
-    implementation(libs.springboot.starter.mail)
-    implementation(libs.springboot.starter.thymeleaf)
-    implementation(libs.springboot.starter.validation)
+    implementation(libs.bundles.springboot) // springboot bundle
 
     implementation(libs.kotlin.reflect)
     implementation(libs.springdoc.openapi.starter.webmvc.ui)
@@ -26,8 +21,4 @@ dependencies {
     implementation(libs.mapstruct.api)
 
     kapt(libs.mapstruct.kapt)
-}
-
-kapt {
-    keepJavacAnnotationProcessors = true
 }
