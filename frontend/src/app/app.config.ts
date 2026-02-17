6bfa6887221307e01726e64286e6ec209d410d5f
Valentin MILLET <valentin.millet39@gmail.com>
2026-02-17 01:05:31 +0100

    Migrate from karma jasmine to vitest and use vite as builder

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, inject, provideAppInitializer, provideZonelessChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {routes} from './app.routes';
@@ -9,7 +9,7 @@ import {firstValueFrom} from "rxjs";
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideZoneChangeDetection({eventCoalescing: true}),
+        provideZonelessChangeDetection(),
         provideRouter(routes),
         provideHttpClient(withInterceptors([authInterceptor])),
         ...translocoProviders,
--- frontend/src/app/transloco/transloco-root.module.ts
+++ frontend/src/app/transloco/transloco-root.module.ts
@@ -7,7 +7,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+      return this.http.get<Translation>(`/i18n/${lang}.json`);
   }
 }
 
