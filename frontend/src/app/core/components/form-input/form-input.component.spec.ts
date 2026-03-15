5dca6b56a37f825ffab45bfdead6a4e69a28ae1b
Valentin Millet <valentin.millet39@gmail.com>
2026-03-15 17:59:35 +0100

    fix TU

--- frontend/src/app/core/components/form-input/form-input.component.spec.ts
+++ frontend/src/app/core/components/form-input/form-input.component.spec.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FormInputComponent } from './form-input.component';
+import {FormInputComponent} from './form-input.component';
+import {getTranslocoModule} from "../../../transloco/testing/transloco-testing.module";
 
 describe('FormInputComponent', () => {
-  let component: FormInputComponent;
-  let fixture: ComponentFixture<FormInputComponent>;
+    let component: FormInputComponent;
+    let fixture: ComponentFixture<FormInputComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FormInputComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FormInputComponent, getTranslocoModule()],
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(FormInputComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+        fixture = TestBed.createComponent(FormInputComponent);
+        component = fixture.componentInstance;
+        fixture.componentRef.setInput('id', 'test-id');
+        await fixture.whenStable();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
